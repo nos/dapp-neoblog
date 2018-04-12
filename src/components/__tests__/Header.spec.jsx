8cf0515a6af12e5683bd4aba59a640ccecd428fb
Jeroen Peeters <jeroen.peeters@evance.be>
2018-04-12 15:45:40 +0200

    chore(test): remove unused tests

--- src/components/__tests__/Header.spec.jsx
+++ src/components/__tests__/Header.spec.jsx
@@ -8,13 +8,13 @@ import Header from "./../Header";
 describe("Components", () => {
   describe("Header", () => {
     test("should render", () => {
-      const wrapper = render(<Header title="Hello world!" />);
+      const wrapper = render(<Header title="Hello" subTitle="world!" />);
       expect(wrapper).toMatchSnapshot();
     });
 
     test("should validate props", () => {
-      TestPropTypes(<Header title="Hello world!" />, 0);
-      TestPropTypes(<Header />, 1);
+      TestPropTypes(<Header title="Hello" subTitle="world!" />, 0);
+      TestPropTypes(<Header />, 2);
     });
   });
 });
